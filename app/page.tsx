1359c0f11590a74d3dfac708b054573d6b2b1ab7
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2024-03-22 08:59:55 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main>
+    <section className={"main"}>
+      HOME PAGE
+    </section>
+    </main>
+  );
+}
